955da405bb78a5f084e330b0dbc037759f149086
Garrett Hanberg <115447682+garretthanberg@users.noreply.github.com>
2023-08-01 14:04:50 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/compat";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCdtUyQrs-IP6Zt6RMZKwN3MYMnOjvgzbk",
-  authDomain: "rn-instagram-clone-gh.firebaseapp.com",
+  apiKey: "",
+  authDomain: "",
   projectId: "rn-instagram-clone-gh",
-  storageBucket: "rn-instagram-clone-gh.appspot.com",
-  messagingSenderId: "1050575719625",
-  appId: "1:1050575719625:web:b24c9b08cd2bb2d9299118",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
